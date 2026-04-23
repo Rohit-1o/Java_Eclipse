7f87dae1ae8d05c2d588ea726c6b0d053f14952a
rohitt <164724029+Rohit-1o@users.noreply.github.com>
2026-04-23 19:20:55 +0530

    method

--- Assignments/src/StockDataRetrieval.java
+++ Assignments/src/StockDataRetrieval.java
@@ -1,6 +1,6 @@
 
 public class StockDataRetrieval implements DataRetrieval {
-	   @Override
+	   
 	    public String retrieveData(int id) {
 	        return "Stock data retrieved for ID: " + id;
 	    }
